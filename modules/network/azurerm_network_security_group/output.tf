bbbf41c675561f83b272b79a71800e2c1dded60b
root <surajphawade@gmail.com>
2025-12-25 20:40:15 +0100

    NSG Code added with rule

---
+++ modules/network/azurerm_network_security_group/output.tf
@@ -0,0 +1,7 @@
+output "network_security_group_name" {
+  value = azurerm_network_security_group.this.name
+}
+
+output "network_security_group_id" {
+  value = azurerm_network_security_group.this.id
+}
